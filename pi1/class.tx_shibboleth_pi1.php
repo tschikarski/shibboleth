96f258e1802ef3c29a5865d32efef971c2b9f46b
tschikarski <tschikarski@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-02 09:34:34 +0000

    Introduced a test link for Shibboleth Session-Initiation, changed .htaccess to reflect "lazy mode".
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/shibboleth/trunk@37543 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_shibboleth_pi1.php
+++ pi1/class.tx_shibboleth_pi1.php
@@ -57,17 +57,7 @@ class tx_shibboleth_pi1 extends tslib_pibase {
 		
 	
 		$content='
-			<strong>This is a few paragraphs:</strong><br />
-			<p>This is line 1</p>
-			<p>This is line 2</p>
-	
-			<h3>This is a form:</h3>
-			<form action="'.$this->pi_getPageLink($GLOBALS['TSFE']->id).'" method="POST">
-				<input type="text" name="'.$this->prefixId.'[input_field]" value="'.htmlspecialchars($this->piVars['input_field']).'">
-				<input type="submit" name="'.$this->prefixId.'[submit_button]" value="'.htmlspecialchars($this->pi_getLL('submit_button_label')).'">
-			</form>
-			<br />
-			<p>You can click here to '.$this->pi_linkToPage('get to this page again',$GLOBALS['TSFE']->id).'</p>
+			<a href="http://testshibb.abezetdom.local/Shibboleth.sso/TestShib?target=http%3A%2F%2Ftestshibb.abezetdom.local%2Fphpinfo.php">Shibboleth-Login</a>
 		';
 	
 		return $this->pi_wrapInBaseClass($content);
