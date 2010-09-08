906dd3040b17d90cb32a07981019deb041399929
skorpi <skorpi@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-08 15:41:03 +0000

    * added some installation guidelines to the manual * switched the domain in the example shibboleth2.xml * added a little flexibility to the link-generation (login-link)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/shibboleth/trunk@37831 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_shibboleth_pi1.php
+++ pi1/class.tx_shibboleth_pi1.php
@@ -54,10 +54,15 @@ class tx_shibboleth_pi1 extends tslib_pibase {
 		$this->conf = $conf;
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
+		global $TYPO3_CONF_VARS;
 		
-	
+		$extConf = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['shibboleth']);	
+		// TODO: check slashes of conf-urls
+		// TODO: https
+		// TODO: urlencode des target
+		
 		$content='
-			<a href="http://testshibb.abezetdom.local/Shibboleth.sso/TestShib?target=http%3A%2F%2Ftestshibb.abezetdom.local">Shibboleth-Login</a>
+			<a href="http://' . t3lib_div::getIndpEnv('HTTP_HOST') . '' . $extConf['sessions_handlerURL'] . $extConf['sessionInitiator_Location'] . '?target=http%3A%2F%2F' . t3lib_div::getIndpEnv('HTTP_HOST') . '">Shibboleth-Login</a>
 		';
 	
 		return $this->pi_wrapInBaseClass($content);
