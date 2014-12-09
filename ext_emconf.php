3ba5dfa9b34e13289378ca49fd371ad850dca346
Thomas Schikarski <thomas.schikarski@trusting-connections.net>
2014-12-09 10:43:51 +0100

    Update ext_emconf.php to reflect lost compatibility with TYPO3 ver. 4

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
