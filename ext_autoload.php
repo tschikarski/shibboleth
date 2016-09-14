a19b67f043bfad5781898ec221ec36f374931e9e
Thomas Schikarski <thomas.schikarski@trusting-connections.net>
2016-09-14 14:20:50 +0200

    Remove obsolete file "ext_autoload.php"

--- ext_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-die('RUN INTO ext_autoload.php, which is not up to date!');
-$extpath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('shibboleth');
-return array(
-	'TrustCnct\\Shibboleth\\tx_shibboleth_sv1' => $extpath . 'sv1/class.tx_shibboleth_sv1.php',
-    'TrustCnct\\Shibboleth\\User\\UserHandler' => $extpath . 'Classes/User/UserHandler.php',
-	'tx_shibboleth_toolbar' => $extpath . 'hooks/class.tx_shibboleth_toolbar.php',
-);
