45d2447bd4f9a03b5e070a83d890e5f6f1c458f5
Thomas Schikarski <thomas.schikarski@trusting-connections.net>
2017-11-02 16:57:37 +0100

    [FIX] Faulty plugin registration

--- ext_tables.php
+++ ext_tables.php
@@ -3,3 +3,9 @@
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'TrustCnct.Shibboleth',
+    'LoginLink',
+    'Shibboleth Login Link'
+);
