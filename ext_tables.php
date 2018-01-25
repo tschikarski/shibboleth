c45f5d82be1afeb30fcce7786aa7a3f777f57db5
Thomas Schikarski <thomas.schikarski@trusting-connections.net>
2018-01-25 15:02:35 +0100

    [FIX] Remove Installtool message "TCA in ext_tables.php check" for shibboleth ext

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,8 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'TrustCnct.Shibboleth',
-    'LoginLink',
-    'Shibboleth Login Link'
-);
+//\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+//    'TrustCnct.Shibboleth',
+//    'LoginLink',
+//    'Shibboleth Login Link'
+//);
